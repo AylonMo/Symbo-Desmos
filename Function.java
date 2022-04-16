98bd94f953e48d83e427eb097a5672b74c95fd0c
Aylon Moyal <aylon.moyal@gmail.com>
2022-04-17 02:03:01 +0300

    Update function roots algorithm and input scanning

--- Function.java
+++ Function.java
@@ -1,18 +1,60 @@
+import java.util.Arrays;
+import java.util.Random;
+
+
 public class Function {
+    private static int APPROX_DEPTH = 10000;
+    private static double ROUNDING_DIST = 0.0001;
     private double[] coefficients;
 
     public Function(double[] coefficients) {
         this.coefficients = coefficients;
     }
 
+    // ——————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
+    public int getDegree() {
+        return coefficients.length - 1;
+    }
+    
+    // representation of the function, printed user-friendly
     public String toString() {
-        String repr = "f(x) = ";
-        for (int i = coefficients.length - 1; i >= 0; i--)
-            repr += coefficients[i] + " * x^" + i + " + ";
-        return repr.substring(0, repr.length() - 3);
+        if (getDegree() == 0)
+            return "" + coefficients[0];
+        
+        String repr = "f(x) = ", sign;
+        String xExpression = "x" + ((getDegree() == 1)? (""):("^" + getDegree()));
+        
+        // if the coefficient is 1 or -1
+        if (smartRounding(Math.abs(coefficients[getDegree()])) == 1)
+            repr += xExpression;
+        else
+            repr += coefficients[getDegree()] + " * " + xExpression;
+        
+        for (int i = getDegree() - 1; i >= 1; i--) {
+            if (coefficients[i] == 0) continue;
+            
+            // add to repr the correct form of the coefficient * x ^ i
+            xExpression = "x" + ((i == 1)? (""):("^" + i));
+            sign = (coefficients[i] < 0)? (" - "):(" + ");
+
+            // if the coefficient is 1 or -1
+            double roundedCoeff = smartRounding(Math.abs(coefficients[i]));
+            if (roundedCoeff == 1)
+                repr += sign + xExpression;
+            else
+                repr += sign + smartRounding(Math.abs(coefficients[i])) + " * " + xExpression;
+        }
+
+        // add the constant of the function
+        if (coefficients[0] != 0)
+            repr += ((coefficients[0] < 0)? (" - "):(" + ")) + smartRounding(Math.abs(coefficients[0]));
+        return repr;
 	}
 
+    // ——————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+    // a method to calculate f(x) for a given x
     public double calcValue(double x) {
         double result = 0;
 		for (int i = 0; i < coefficients.length; i++)
@@ -20,15 +62,180 @@ public class Function {
 		return result;
 	}
 
-    public void printPoint(double x) {
-		System.out.println("(" + x + ", " + calcValue(x) + ")");
-	}
-
+    // a method to get the derivative of the function
     public Function calcDerivative() {
         double[] derCoeffs = new double[coefficients.length - 1];
 		for (int i = 0; i < derCoeffs.length; i++)
 			derCoeffs[i] = (i + 1) * coefficients[i + 1];
 		return new Function(derCoeffs);
 	}
+
+    // ——————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+    // a method that returns a rounded value of the parameter without changing it
+    public double smartRounding(double value) {
+        if (Math.abs(Math.round(value) - value) < ROUNDING_DIST)
+            return Math.round(value);
+        return Math.round(value * 100.0) / 100.0;
+    }
+    
+    // modified toString() for arrays using the smartRounding() method
+    public String reprRoundedArray(double[] array) {
+        Arrays.sort(array);
+        if (array.length == 0)
+            return "[]";
+        
+        String arrayRepr = "[";
+        for (double x : array) {
+            // skip values that appeared earlier
+            if (arrayRepr.indexOf("" + x) == -1)
+                arrayRepr += smartRounding(x) + ", ";
+        }
+
+        arrayRepr = arrayRepr.substring(0, arrayRepr.length() - 2) + "]";
+        return arrayRepr;
+    }
+
+    // ——————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+    /* a method to approximate a root of the function using the Newton-Raphson technique.
+     * for a given function f(x) we can approximate a root of the function using the following recurrence relation:
+     * an+1 = an - f(an) / f'(an) where f'(x) is the derivative of f(x) and the initial term a0 is randomly chosen.
+     */
+    public double newtonRaphson() {
+        Random rand = new Random();
+        double a0 = (rand.nextDouble() + 1) * 10;
+        
+        Function derivative = calcDerivative();
+        while (derivative.calcValue(a0) == 0)
+            a0 = (rand.nextDouble() + 1) * 10;
+        double current = a0, next = a0;
+        
+        for (int i = 0; i < APPROX_DEPTH; i++) {
+            next = current - this.calcValue(current) / derivative.calcValue(current);
+            current = next;
+        } return next;
+    }
+
+
+    /* a method to compute the division of f(x) by the factor (x - alpha) and ignoring the remainder.
+     * the division works for any parameter but it is used only when alpha is a root of the function.
+     */
+    public double[] polynomialDivision(double alpha) {
+        return polDivRec(this.coefficients, -alpha, new double[getDegree()]);
+    }
+    
+    /* a recursive method to execute the polynomial division.
+     * for example: f(x) = 6x³ - 41x² + 59x - 20, divided by (x - 5).
+     * notice that f(x) = (6x² - 11x + 4)(x - 5) so we expect the result to be (6x² - 11x + 4).
+     * the technique is exactly like long division but for polynomials:
+     * 
+     *         6x² - 11x + 4
+     *  ——————————————————————
+     *  6x³ - 41x² + 59x - 20 | x - 5
+     *  6x³ - 30x²    |    |
+     *  ——————————    V    |
+     *      - 11x² + 59x   |
+     *      - 11x² + 55x   |
+     *      ————————————   V
+     *                4x - 20
+     *                4x - 20
+     *                ———————
+     *                      0
+     * the remainder is zero because 5 is a root of f(x), and that will be the case when we call this function.
+     */
+    public double[] polDivRec(double[] current, double alpha, double[] resCoeffs) {
+        int pos = current.length - 1;   // == getDegree()
+        if (pos == 0)                   // if the degree is 0 then we finished the division
+            return resCoeffs;
+        
+        
+        double[] divRes = Arrays.copyOf(current, pos);              // divRes is the result of the current division
+        divRes[pos - 1] = current[pos - 1] - current[pos] * alpha;  // subtract (x - a) times the leading coefficient
+
+        resCoeffs[pos - 1] = current[pos];          // update the coefficient of the final result
+        return polDivRec(divRes, alpha, resCoeffs); // continue the division with the current remainder             
+    }
+
+    // ——————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+    // helper method for finding the other roots of a function when one root is given
+    public double[] getRootsWhenExists(double root) {
+        // divide the polynomial by (x - root) and find the roots of the result
+        Function result = new Function(polynomialDivision(root));
+        double[] rootsArray = result.findRoots();
+
+        // add the given root to the roots array and return it
+        rootsArray = Arrays.copyOf(rootsArray, rootsArray.length + 1);
+        rootsArray[rootsArray.length - 1] = root;
+        return rootsArray;
+    }
+
+    /* a method to find the roots of a function with an odd degree.
+     * by a theorem from calculus, every polynomial with real coefficients and an odd degree has a real root
+     * therefore, we can compute this root using the Newton-Raphson technique.
+     */
+    public double[] findRootsOdd() {
+        if (getDegree() == 1)
+            return new double[] {-coefficients[0] / coefficients[1]};
+        
+        double root = newtonRaphson();
+        return getRootsWhenExists(root);
+    }
+
+    /* a method to find the roots of a function with an even degree.
+     * determined by the graphs of polynomial functions with an even degree, there are no roots of the function (intersection
+     * points with the x axis) in the following cases:
+     * 1. all the extrema points (minimum/maximum) are above the axis and the leading coefficient is positive.
+     * 2. all the extrema points (minimum/maximum) are below the axis and the leading coefficient is negative.
+     * otherwise, using the intermediate-value-theorem from calculus we can deduct that there is at least one root
+     * of the function, because there are positive and negative values of the function.
+     */
+    public double[] findRootsEven() {
+        // the extrema points of the function are roots of the derivative
+        double[] extremaPoints = this.calcDerivative().findRoots();
+        double root = 0; boolean existsRoot = false;
+        
+        boolean allPositive = true; boolean allNegative = true;
+        for (double x : extremaPoints) {
+            if (calcValue(x) > 0) {             // the extrema point is above the x axis
+                allNegative = false;
+            } else if (calcValue(x) < 0) {      // the extrema point is below the x axis
+                allPositive = false;
+            } else {                            // the extrema point is a root of the function
+                root = x; existsRoot = true;
+                break;
+            }
+        }
+
+        // if a root was found amongst the extrema points
+        if (existsRoot)
+            return getRootsWhenExists(root);
+        
+        // check if there is a root to function according to the explanation above
+        boolean option1 = (coefficients[getDegree()] < 0 && allPositive);
+        boolean option2 = (coefficients[getDegree()] > 0 && allNegative);
+        existsRoot = option1 || option2 || (!allPositive && !allNegative);
+
+        // if there is a root, find it using Newton-Raphson and the other roots
+        if (existsRoot)
+            return getRootsWhenExists(newtonRaphson());
+        return new double[0];
+    }
+
+    // a method to find the root of the function, using the relevant method from above
+    public double[] findRoots() {
+        if (getDegree() == 0)
+            return new double[0];
+        
+        double[] result;
+        if (getDegree() % 2 == 0)
+            result = findRootsEven();
+        else
+            result = findRootsOdd();
+        
+        Arrays.sort(result);
+        return result;
+    }
 
 }
--- FunctionAnalysis.java
+++ FunctionAnalysis.java
@@ -1,44 +1,59 @@
 import java.util.Scanner;
 
+
 public class FunctionAnalysis {
     private static Scanner scan = new Scanner(System.in);
 	private Function func;
 
+    // the constructor of the class, asks the degree and coefficients and sets func accordingly
 	public FunctionAnalysis() {
         int degree = getDegree();
-        double[] coefficients = new double[getDegree()];
+        double[] coefficients = new double[degree + 1];
         
-        for (int i = 0; i < degree; i++)
-            coefficients[i] = getCoefficient(i);
+        for (int i = 0; i <= degree; i++)
+            coefficients[i] = getCoefficient(degree, i);
 		this.func = new Function(coefficients);
 	}
 
+    // ——————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+    // represents the class by the function that is analyzed
     public String toString() {
-		return func.toString();
+		return "func is: " + func.toString();
 	}
 
+    // a method to print a point in the form (x, f(x))
+    public void printPoint(double x) {
+        double y = func.smartRounding(func.calcValue(x));
+		System.out.println("(" + x + ", " + y + ")");
+	}
 
+    // ——————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+    // a method to get a valid integer for the degree of the polynomial
     public int getDegree() {
 		boolean invalidInput = true;
         int degree = 0;
-        System.out.println("Please enter the degree of the polynomial function to analize: ");
+        System.out.print("\nPlease enter the degree of the polynomial function to analyze: ");
 
         while (invalidInput) {
             String input = scan.nextLine();
-
             try {
                 degree = Integer.parseInt(input);
-                invalidInput = false;
+                if (degree < 0)
+                    System.out.print("This is not a valid value. Please enter a positive integer for the degree: ");
+                else
+                    invalidInput = false;
             } catch (NumberFormatException e) {
-                System.out.print("This is not a valid value. Please enter an integer for the degree: ");
+                System.out.print("This is not a valid value. Please enter a positive integer for the degree: ");
             }
         }
         
         return degree;
     }
 
-
-	public double getCoefficient(int coeffIndex) {
+    // a method to get a valid number for a coefficient of the function
+	public double getCoefficient(int degree, int coeffIndex) {
 		double coefficient = 0;
 		boolean invalidInput = true;
 
@@ -51,10 +66,12 @@ public class FunctionAnalysis {
 
 		while (invalidInput) {
             String input = scan.nextLine();
-
             try {
                 coefficient = Double.parseDouble(input);
-                invalidInput = false;
+                if (coeffIndex == degree && coefficient == 0)
+                    System.out.print("This is not a valid value. Please enter a non-zero number to represent the coefficient: ");
+                else
+                    invalidInput = false;
             } catch (NumberFormatException e) {
                 System.out.print("This is not a valid value. Please enter a number to represent the coefficient: ");
             }
@@ -63,8 +80,10 @@ public class FunctionAnalysis {
         return coefficient;
 	}
 
+    // ——————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
     public void run() {
-        
+
     }
 
 }
